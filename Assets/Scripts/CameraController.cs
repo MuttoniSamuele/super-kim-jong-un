73e6e44e52c09fda63007f47b4c7074e378f4302
mutti <imperialvictor15@gmail.com>
2022-11-04 21:39:55 +0100

    Add CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	[SerializeField] private GameObject kim;
+	[SerializeField] private float offset;
+
+    void Start() {
+		
+    }
+
+    void Update(){
+		Vector3 newPos = new Vector3(
+			kim.transform.position.x + offset,
+			transform.position.y,
+			transform.position.z
+		);
+		transform.position = newPos;
+    }
+}
