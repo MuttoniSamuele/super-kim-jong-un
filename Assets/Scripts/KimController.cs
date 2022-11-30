60f37762327d2cf5f5809c016f41d2c667cf2c8a
mutti <imperialvictor15@gmail.com>
2022-11-30 18:21:04 +0100

    Add level base

--- Assets/Scripts/KimController.cs
+++ Assets/Scripts/KimController.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class KimController : MonoBehaviour {
 	[SerializeField] private float moveSpeed;
 	[SerializeField] private float jumpSpeed;
+	[SerializeField] private float defaultFriction = 0.4f;
 
 	private Rigidbody2D rbody;
+	private Collider2D coll;
+	private float matFriction;
 
     void Start() {
 		rbody = gameObject.GetComponent<Rigidbody2D>();
-    }
+		coll = gameObject.GetComponent<Collider2D>();
+		matFriction = 0;//coll.sharedMaterial.friction;
+	}
 
-    void Update() {
+    void FixedUpdate() {
 		Vector2 newVel = rbody.velocity;
 		bool moveLeft = Input.GetKey(KeyCode.LeftArrow);
 		bool moveRight = Input.GetKey(KeyCode.RightArrow);
 
 		if (moveLeft == moveRight) {
 			newVel.x = 0;
+			rbody.sharedMaterial.friction = defaultFriction;
 		} else {
 			newVel.x = moveRight ? moveSpeed : -moveSpeed;
+			rbody.sharedMaterial.friction = matFriction;
 		}
 
 		if (Input.GetKey(KeyCode.UpArrow) && rbody.velocity.y == 0) {
